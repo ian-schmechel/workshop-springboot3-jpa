1ff06f11c4af08122df14590480958945994162e
Ian Schmechel <ian.schmechelads@gmail.com>
2024-10-16 15:13:53 -0300

    Category

---
+++ src/main/java/com/ian/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.ian.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ian.course.entities.Category;
+import com.ian.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
