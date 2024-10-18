0ddc372b6c1941cdad581c168fd0066445328659
Ian Schmechel <ian.schmechelads@gmail.com>
2024-10-18 10:56:51 -0300

    Product

---
+++ src/main/java/com/ian/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ian.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ian.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
---
+++ src/main/java/com/ian/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.ian.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ian.course.entities.Product;
+import com.ian.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
