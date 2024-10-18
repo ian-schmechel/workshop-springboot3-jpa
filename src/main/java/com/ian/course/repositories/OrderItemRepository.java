929e32adac0966e6a99982701acf6d6edfd3b8ed
Ian Schmechel <ian.schmechelads@gmail.com>
2024-10-18 16:39:29 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/ian/course/repositories/OrderItemRepository.java
+++ src/main/java/com/ian/course/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.ian.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ian.course.entities.OrderItem;
+import com.ian.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
 
 }
