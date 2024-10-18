16ac19756181f9308dd130a5a09241c5c94b9d46
Ian Schmechel <ian.schmechelads@gmail.com>
2024-10-18 16:51:22 -0300

    Payment, one-to-one association

--- src/main/java/com/ian/course/config/TestConfig.java
+++ src/main/java/com/ian/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ian.course.entities.Category;
 import com.ian.course.entities.Order;
 import com.ian.course.entities.OrderItem;
+import com.ian.course.entities.Payment;
 import com.ian.course.entities.Product;
 import com.ian.course.entities.User;
 import com.ian.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
